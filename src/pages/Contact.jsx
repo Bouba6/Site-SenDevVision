fa7485dd3d9f30e009feaf82e85b4c45b971f08a
Fairyghost007 <biranenini6762@gmail.com>
2025-06-23 23:37:47 +0000

    ✨feat:implemented the contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import { ContactSection } from "../components/ContactSection";
+import { Toaster } from "react-hot-toast";
+
+const ContactPage = () => {
+  return (
+    <>
+      <Toaster />
+      <ContactSection />
+    </>
+  );
+};
+
+export default ContactPage;
